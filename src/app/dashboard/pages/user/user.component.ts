d3279a2c3cd54c71c1af0aa6a267f64ed6266a0e
fredy-martinez <fredymartinez18811@gmail.com>
2024-02-06 21:38:59 -0600

    Termine seccion Angular 17

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../../interface/req-response';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [TitleComponent, CommonModule],
+  template: `
+    <app-title [title]="titleLabel()"></app-title>
+
+    @if(user()){
+
+    <section>
+      <img [srcset]="user()!.avatar" [alt]="user()!.first_name" />
+
+      <div>
+        <h3>{{ user()!.first_name }} {{ user()!.last_name }}</h3>
+        <p>{{ user()!.email }}</p>
+      </div>
+    </section>
+    }@else {
+    <p>Cargando Información...</p>
+    }
+  `,
 })
 export class UserComponent {
+  private route = inject(ActivatedRoute);
+  private userService = inject(UsersService);
 
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap(({ id }) => this.userService.getUserById(id))
+    )
+  );
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `Información del usuario ${this.user()!.first_name} ${
+        this.user()!.last_name
+      }`;
+    }
+    return 'Información del usuario';
+  });
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,11 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { User, UsersResponse } from '../interface/req-response';
+import type {
+  User,
+  UserResponse,
+  UsersResponse,
+} from '../interface/req-response';
 import { HttpClient } from '@angular/common/http';
-import { delay } from 'rxjs';
+import { delay, map } from 'rxjs';
 
 interface State {
   users: User[];
@@ -9,32 +13,38 @@ interface State {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
-  private http = inject( HttpClient);
+  private http = inject(HttpClient);
 
   #state = signal<State>({
     loading: true,
     users: [],
-  })
+  });
 
   //Señales computadas para usar ese http privado en el typescript
-  public users = computed( () => this.#state().users);
-  public loading = computed( () => this.#state().loading);
+  public users = computed(() => this.#state().users);
+  public loading = computed(() => this.#state().loading);
 
-  constructor() { 
-
-    this.http.get<UsersResponse>('https://reqres.in/api/users')
-      .pipe( delay(1500))
-      .subscribe( res => {
+  constructor() {
+    this.http
+      .get<UsersResponse>('https://reqres.in/api/users')
+      .pipe(delay(1500))
+      .subscribe((res) => {
         this.#state.set({
           loading: false,
           users: res.data,
-        })
-      })
-    
+        });
+      });
   }
 
+  getUserById(id: string) {
+    return this.http
+      .get<UserResponse>(`https://reqres.in/api/users/${id}`)
+      .pipe(
+        delay(1500),
+        map((resp) => resp.data)
+      );
+  }
 }
